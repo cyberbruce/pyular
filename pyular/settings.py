fc2336dffcbb70fb64588e1ce341cdc3781404bd
Bruce Martin <cyber.bruce@yahoo.com>
2022-06-27 22:36:57 -0700

    whitenoise

--- pyular/settings.py
+++ pyular/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s_vqc&-t(#sr5tg#b--%$+p#)mjb3*e!1n709gk3k5rg0t*@bb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-env.eba-bkqdiip4.us-west-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['django-env.eba-bkqdiip4.us-west-1.elasticbeanstalk.com','127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -115,8 +116,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+STATIC_ROOT = 'static/'
 STATIC_URL = 'static/'
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
