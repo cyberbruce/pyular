880b2bc1b6bb77016d07b182cc097ae5ae3f7b4f
Bruce Martin <cyber.bruce@yahoo.com>
2022-06-30 21:04:44 -0700

    apache

--- pyular/settings.py
+++ pyular/settings.py
@@ -125,3 +125,6 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#SECURE_SSL_REDIRECT = True
