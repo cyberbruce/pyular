a465375dbba8a3b51e15362536a80921d8362284
Bruce Martin <cyber.bruce@yahoo.com>
2022-06-30 21:37:19 -0700

    working on deploy

--- pyular/settings.py
+++ pyular/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,5 +127,5 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-#SECURE_SSL_REDIRECT = True
+if os.getenv("DJANGO_ENV") == "production":
+    SECURE_SSL_REDIRECT = True
